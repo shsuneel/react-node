679f64869b6308e974b0098ff50e9d9d22c00be2
LAPTOP-QSODT5VR\Suneel <shsuneel@gmail.com>
2018-04-25 00:13:33 +0100

    initial structure

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../assets/css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Coming Soon allll</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
